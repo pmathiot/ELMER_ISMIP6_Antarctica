454e16701b906a8f000beae400a04b588fe3120d
Pierre MATHIOT <mathiotp@irene194.c-irene.mg1.tgcc.ccc.cea.fr>
2022-07-05 14:59:05 +0200

    clean SlipCoef.F90

--- MY_SRC/SlipCoef.F90
+++ MY_SRC/SlipCoef.F90
@@ -19,8 +19,8 @@ FUNCTION CalculSlc_haf (model, nodenumber, VarIn) RESULT(VarOut)
  ENDIF
 
  ! get needed variable
- hth = ListGetConstReal( Material, 'SSA Friction Threshold Height',UnFoundFatal=.TRUE.) 
- zsl = ListGetCReal( Model % Constants, 'Sea Level',UnFoundFatal=.TRUE.)
+ hth  = ListGetConstReal( Material, 'SSA Friction Threshold Height',UnFoundFatal=.TRUE.) 
+ zsl  = ListGetCReal( Model % Constants, 'Sea Level',UnFoundFatal=.TRUE.)
  rhoi = ListGetCReal( Model % Constants, 'Ice density',UnFoundFatal=.TRUE.)
  rhow = ListGetCReal( Model % Constants, 'water density',UnFoundFatal=.TRUE.)
 
@@ -30,38 +30,25 @@ FUNCTION CalculSlc_haf (model, nodenumber, VarIn) RESULT(VarOut)
  !    h_af = zs - freeboard
  h_af = VarIn(2) - ((rhow/rhoi)*(zsl - VarIn(3))+VarIn(3))
 
- ! PM : h_af_init commented as not used 
- !h_af = VarIn(2) - ((rhow/rhoi)*(zsl - VarIn(4))+VarIn(4))
- !h_af_init = MAX(0.1, VarIn(3) - ((rhow/rhoi)*(zsl - VarIn(4))+VarIn(4)))
- ! end PM
-
- !IF (h_af.GT.75.) THEN
- ! lda=+1.0
- !ELSE
- ! VarOut=h_af/MIN(75.0,h_af_init)
- !ENDIF
-
  ! compute scale scale factor  
  !     0 on floating part
  !     1 where above treshold
  ! [0-1] where between free floating and treshold
- !lda=h_af/MIN(h_af_init,hth)
 
  ! general case
  lda=h_af/hth
 
- ! where floating 
+ ! where floating => 0
  IF (lda.LE.0.) THEN
- lda=+0.0
+    lda=+0.0
  ENDIF
 
- ! haf superior to h_threshold 
+ ! haf superior to h_threshold => 1
  IF (h_af.GE.hth) THEN
  lda=+1.0
  ENDIF
 
- ! output
- ! transition between both case)
+ ! output : slc corrected by Haf (ie close to GL, we decrease slc)
  VarOut=VarIn(1)*lda
  
  END FUNCTION CalculSlc_haf
