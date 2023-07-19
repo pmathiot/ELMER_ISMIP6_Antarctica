f9b7656dc9bfa90eff25a5376dc1df475d69f0b9
Justine CAILLET <cailletj@irene194.c-irene.mg1.tgcc.ccc.cea.fr>
2023-07-19 17:56:28 +0200

    update of friction choice + epsg correction

--- prepare_elmer.bash
+++ prepare_elmer.bash
@@ -20,41 +20,6 @@ if [ ! -d SELMER  ]; then mkdir -p  $SELMER   ; fi
 # source run param file
 . run_param.bash
 
-## create Friction_Material
-FRICTION_lc=$(echo "$FRICTION" | tr '[:upper:]' '[:lower:]')
-case "$FRICTION_lc" in
-        linear)
-                echo "SSA Friction Law = String \"linear\"" > $WELMER/Friction_Material.sif || exit 42
-                echo "SSA Friction Parameter = Equals frC" >> $WELMER/Friction_Material.sif || exit 42
-                XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_linear_friction_coefficent\" unit=\"MPa m-1 d\"    grid_ref=\"GridNodes\" \/\>"
-                ;;
-	weertman)
-		echo "SSA Friction Law = String \"Weertman\"" > $WELMER/Friction_Material.sif || exit 42
-		echo "SSA Friction Parameter = Equals frC" >> $WELMER/Friction_Material.sif || exit 42
-		echo "SSA Friction Exponent = Real $1.0/n" >> $WELMER/Friction_Material.sif || exit 42
-		echo "SSA Friction Linear Velocity = Real $Vmin" >> $WELMER/Friction_Material.sif || exit 42
-		parsed_exp=$(echo $1.0/n | sed 's/[$# ]//g;s@/@\\/@g' )
-		XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_weertman_friction_coefficent\" unit=\"MPa m^-$parsed_exp d^$parsed_exp\"    grid_ref=\"GridNodes\" \/\>"
-		;;
-	"regularized coulomb")
-		echo "SSA Friction Law = String \"Regularized coulomb\"" > $WELMER/Friction_Material.sif || exit 42
-		echo "SSA Friction Parameter = Variable frC, haf" >> $WELMER/Friction_Material.sif || exit 42
-		echo "   Real Procedure "SlipCoef" "Calcul_Slc"" >> $WELMER/Friction_Material.sif || exit 42
-                echo "SSA Friction Exponent = Real $1.0/n" >> $WELMER/Friction_Material.sif || exit 42
-                echo "SSA Friction Linear Velocity = Real $Vmin" >> $WELMER/Friction_Material.sif || exit 42
-                echo "SSA Friction Threshold Velocity=Real $Vthres" >> $WELMER/Friction_Material.sif  || exit 42
-		echo "SSA Friction Threshold Height=Real $Hthres" >> $WELMER/Friction_Material.sif  || exit 42
-                XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_RegCoulomb_friction_coefficent\" unit=\"MPa\"    grid_ref=\"GridNodes\" \/\>"
-		;;
-
-        *)  echo "Sorry Friction Law not ready; exit"; exit 42
-esac
-
-echo "----------------------------------------------------"
-echo "Friction law definition :"
-cat $WELMER/Friction_Material.sif
-echo "----------------------------------------------------"
-
 
 # to fix issue for vtu group gidbit
 chgrp -R ${GROUPUSR} ${WELMER}
@@ -89,7 +54,7 @@ echo
 \cp $CONFIG-${CASE}_elmer.param  $WELMER/elmer.param
 \cp $CONFIG-${CASE}_elmer.incf   $WELMER/elmer.incf
 \cp iodef_elmer.xml              $WELMER/.
-\cp context_elmer.xml            $WELMER/.
+#\cp context_elmer.xml            $WELMER/.
 \cp iodef_pp.xml                 $WELMER/.
 \cp context_pp.xml               $WELMER/.
 \cp $CONFIG-${CASE}_pp.sif               $WELMER/pp.sif
@@ -165,9 +130,50 @@ do
         -e "s!<ID>!${i}!g"               \
         -e "s!<ID-1>!${im1}!g"           \
         -e "s!<ID+1>!${ip1}!g"         run_elmer_skel.bash >> run_elmer_${i}.slurm
-    
+   
+
+
+
+
+    ## create Friction_Material
+    FRICTION_lc=$(echo "$FRICTION" | tr '[:upper:]' '[:lower:]')
+    case "$FRICTION_lc" in
+             linear)
+                    echo "SSA Friction Law = String \"linear\"" > $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Parameter = Equals frC" >> $WELMER/Friction_Material.sif || exit 42
+                    XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_linear_friction_coefficent\" unit=\"MPa m-1 d\"    grid_ref=\"GridNodes\" \/\>"
+                    ;;
+             weertman)
+                    echo "SSA Friction Law = String \"Weertman\"" > $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Parameter = Equals frC" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Exponent = Real $ 1.0/n" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Linear Velocity = Real $ Vmin" >> $WELMER/Friction_Material.sif || exit 42
+                    n=$(\echo $(\grep "^ *\$n *= *[0-9]" ANT50.GL1-ISMIP6_elmer.param | \cut -d= -f2 | \cut -d! -f1) | \sed s/.0$//g)
+		    echo "n = $n"
+		    XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_weertman_friction_coefficent\" unit=\"MPa m^-1/$n d^1/$n\"    grid_ref=\"GridNodes\" \/\>"
+                    ;;
+             "regularized coulomb")
+                    echo "SSA Friction Law = String \"Regularized coulomb\"" > $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Parameter = Variable frC, haf" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "   Real Procedure "SlipCoef" "Calcul_Slc"" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Exponent = Real $ 1.0/n" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Linear Velocity = Real $ Vmin" >> $WELMER/Friction_Material.sif || exit 42
+                    echo "SSA Friction Threshold Velocity=Real $ Vthres" >> $WELMER/Friction_Material.sif  || exit 42
+                    echo "SSA Friction Threshold Height=Real $ Hthres" >> $WELMER/Friction_Material.sif  || exit 42
+                    XIOS_frc_def="\<field id=\"frc\"  long_name=\"ssa_RegCoulomb_friction_coefficent\" unit=\"MPa\"    grid_ref=\"GridNodes\" \/\>"
+                    ;;
+ 
+              *)  echo "Sorry Friction Law not ready; exit"; exit 42
+    esac
+
+    echo "----------------------------------------------------"
+    echo "Friction law definition :"
+    cat $WELMER/Friction_Material.sif
+    echo "----------------------------------------------------"
+
+
     # prepare xml file
-    sed "s/<FRICTION_FIELD>/$XIOS_frc_def/g" context_elmer.xml > $WELMER/context_elmer.xml || exit 42
+    sed "s@<FRICTION_FIELD>@$XIOS_frc_def@g" context_elmer.xml > $WELMER/context_elmer.xml || exit 42
     # manage status file
     if [[ $((ijob)) == 1 ]];then touch ${HELMER}/zELMER_${i}_READY; fi
 
